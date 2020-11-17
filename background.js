aa7a65a658906da42e055e1f9cbfae292f6654f1
Joel Carlos <joelc@peplink.com>
2020-11-17 10:20:07 +0800

    menu

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-let list = [];
+
 ("use strict");
 chrome.runtime.onInstalled.addListener(function () {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
@@ -12,20 +12,60 @@ chrome.runtime.onInstalled.addListener(function () {
       },
     ]);
   });
-
-  function start() {
-    const tab = list.pop();
-    chrome.tabs.get(tab.id, function (tab) {
-      chrome.tabs.highlight({ tabs: tab.index }, function () {
-        setTimeout(start, 1000);
-      });
-    });
-  }
-
-  chrome.browserAction.onClicked.addListener(function () {
-    chrome.tabs.query({ active: false }, function (tabs) {
-      list = tabs;
-      start();
-    });
-  });
 });
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(request);
+  if (request.msg == "startCycleTabs") {
+    hancleCycleTabs();
+  }
+});
+
+const hancleCycleTabs = () => {
+  let list = [];
+  chrome.tabs.query({ active: false, currentWindow: true }, function (tabs) {
+    console.log(tabs);
+    function start() {
+      if (list.length === 0) return false;
+
+      const tab = list.pop();
+      chrome.tabs.get(tab.id, function (tab) {
+        try {
+          chrome.tabs.highlight({ tabs: tab.index }, function () {
+            setTimeout(start, 300);
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      });
+    }
+
+    list = tabs;
+    start();
+  });
+};
+
+// chrome.browserAction.onClicked.addListener(function () {
+//   console.log("click");
+//   let list = [];
+//   chrome.tabs.query({ active: false }, function (tabs) {
+//     function start() {
+//       if (list.length === 0) return false;
+
+//       const tab = list.pop();
+//       chrome.tabs.get(tab.id, function (tab) {
+//         try {
+//           chrome.tabs.highlight({ tabs: tab.index }, function () {
+//             setTimeout(start, 1000);
+//           });
+//         } catch (err) {
+//           console.log(err);
+//         }
+//       });
+//     }
+
+//     list = tabs;
+//     console.log(tabs);
+//     start();
+//   });
+// });
--- popup.js
+++ popup.js
@@ -4,14 +4,59 @@
 
 "use strict";
 
-// let button = document.getElementById("start");
-// button.addEventListener("click", function (element) {
-//   chrome.tabs.query({ active: false }, function (tabs) {
-//     const tab = tabs[0];
-//     console.log(tabs);
-//     chrome.tabs.get(tab.id, function (tab) {
-//       chrome.tabs.highlight({ tabs: tab.index }, function () {});
-//     });
-//   });
-// });
+const moveTab = (query) => {
+  chrome.windows.create((window) => {
+    chrome.tabs.query(query, function (tabs) {
+      chrome.tabs.move(
+        tabs.map((i) => i.id),
+        { windowId: window.id, index: 0 },
+        (e) => {
+          console.log(e, "callback");
+        }
+      );
+    });
+    chrome.tabs.query({ title: "New Tab" }, (tabs) => {
+      chrome.tabs.remove(tabs.map((i) => i.id));
+    });
+  });
+};
 
+let gleam = document.querySelector("#gleam");
+gleam.addEventListener("click", () => {
+  moveTab({ url: "https://gleam.io/*" });
+});
+
+let discord = document.querySelector("#discord");
+discord.addEventListener("click", () => {
+  moveTab({ url: "https://discord.com/*" });
+});
+
+let twitter = document.querySelector("#twitter");
+twitter.addEventListener("click", () => {
+  moveTab({ url: "https://twitter.com/*" });
+});
+
+let playr = document.querySelector("#playr");
+playr.addEventListener("click", () => {
+  moveTab({ url: "https://playr.gg/giveaway/*" });
+});
+
+let rafflecopter = document.querySelector("#rafflecopter");
+rafflecopter.addEventListener("click", () => {
+  moveTab({ url: "https://www.rafflecopter.com/*" });
+});
+
+let bookbub = document.querySelector("#bookbub");
+bookbub.addEventListener("click", () => {
+  moveTab({ url: "https://www.bookbub.com/authors/*" });
+});
+
+let youtube = document.querySelector("#youtube");
+youtube.addEventListener("click", () => {
+  moveTab({ url: "https://www.youtube.com/*" });
+});
+
+let cycleTabs = document.querySelector("#cycleTabs");
+cycleTabs.addEventListener("click", () => {
+  chrome.runtime.sendMessage({ msg: "startCycleTabs" });
+});
